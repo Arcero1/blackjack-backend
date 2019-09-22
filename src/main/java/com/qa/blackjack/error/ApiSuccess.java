9d4bfe8bb9ce64298ad2369c696d6db732c7b643
Bartek Marcysiak <bartekmarcysiak@gmail.com>
2019-09-22 15:19:15 +0100

    added overload to ApiSuccess.java constructor

--- src/main/java/com/qa/blackjack/error/ApiSuccess.java
+++ src/main/java/com/qa/blackjack/error/ApiSuccess.java
@@ -3,6 +3,10 @@ package com.qa.blackjack.error;
 import com.qa.blackjack.util.ApiStatus;
 
 public class ApiSuccess extends ApiResponse {
+
+    public ApiSuccess(ApiStatus status, String message) {
+        super(200, status, message);
+    }
     public ApiSuccess(String message) {
         super(200, ApiStatus.SUCCESS, message);
     }
