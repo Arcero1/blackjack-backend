7682ac5636429d86762e7a095886dd43e370c6c3
Bartek Marcysiak <bartekmarcysiak@gmail.com>
2019-09-25 21:53:47 +0100

    changed README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-## BlackJack
-### Backend Application
+## BlackJack: An Application
+This repository, along with its frontend counterpart found [here](https://github.com/Arcero1/blackjack-frontend),
+is my submission for the QA Academy Individual Project.
+
+
+## 1. INTRODUCTION
+### 1a. Motivation
+The brief was to create a simple implementation of an online CRUD database. 
+However, due to the open-ended nature of the project, coupled with 
+the creator's distaste for data entry, a basic database was out of the question
+before it was considered.
+ 
+As throughout the first two weeks in the academy,
+the creator had already created two separate "blackjack"-ish functions,
+he considered himself something of an expert in blackjack.
+
+Because of this, as well as a truly pitiful lack of online blackjack applications, 
+the idea for this project was devised.
+
+### 1b. The Goal
+The main aim of the project was the creation of a full-stack implementation of an online game of blackjack.
+Ultimately, such an implementation would be played just as blackjack would in a casino (save the gambling real money part).
+
+The Goal at large can be split up into the following aims:
+* Create a profile with 
+
+## 4. CONCLUSION
+### 4a. The Overall Result
+
+### 4b. Future Development
+
--- src/test/java/selenium/IncorrectUsageTest.java
+++ src/test/java/selenium/IncorrectUsageTest.java
@@ -2,6 +2,7 @@
 //
 //import org.junit.After;
 //import org.junit.Before;
+//import org.junit.BeforeClass;
 //import org.junit.Test;
 //import org.openqa.selenium.By;
 //import org.openqa.selenium.WebElement;
@@ -9,24 +10,33 @@
 //
 //import static org.junit.Assert.assertTrue;
 //
-//public class GoogleSeleniumTest {
-//    private ChromeDriver driver;
+//public class IncorrectUsageTest {
+//    private static ChromeDriver driver;
 //
-//    @Before
-//    public void setup() {
+//    @BeforeClass
+//    public static void setup() {
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arcero\\blackjack-backend\\src\\test\\resources\\chromedriver.exe");
 //        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("http://35.205.127.204/");
+//        sleep(2000);
+//    }
+//
+//    @Before
+//    public void setUpTest() {
+//        driver.getSessionStorage().clear();
+//        driver.getLocalStorage().clear();
+//        driver.navigate().refresh();
+//        sleep(2000);
 //    }
 //
 //    @Test
-//    public void createAccountTest() {
-//        driver.manage().window().maximize();
-//        driver.get("http://35.205.127.204/");
-//        sleep(5000);
-//
+//    public void testTryToStartWithoutAnAccount() {
+//        driver.findElementById("").click();
+//        assertTrue();
 //    }
 //
-//    private void sleep(long millis) {
+//    private static void sleep(long millis) {
 //        long original = System.currentTimeMillis();
 //        while (true) {
 //            if (System.currentTimeMillis() - original >= millis) {
