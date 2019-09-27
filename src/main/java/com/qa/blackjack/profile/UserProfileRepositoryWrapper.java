44913d3236422f317cbb453e03803726980fb02a
B Marcysiak <bartekmarcysiak@gmail.com>
2019-09-27 09:40:17 +0100

    Merge branch 'dev' of C:\Users\Admin\IdeaProjects\blackjack-backend with conflicts.

--- src/main/java/com/qa/blackjack/profile/UserProfileRepositoryWrapper.java
+++ src/main/java/com/qa/blackjack/profile/UserProfileRepositoryWrapper.java
@@ -2,48 +2,50 @@ package com.qa.blackjack.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * This class wraps UserProfileRepository
- * purpose: decreasing test effort
- *
- * @author BMarcysiak
- * @version 0.1
- */
-@Component
-public class UserProfileRepositoryWrapper {
+@RestController
+public class UserProfileWrapper {
     private UserProfileRepository repository;
 
+    @Transactional
     public void save(UserProfile profile) {
         repository.save(profile);
     }
 
+    @Transactional
     public List<UserProfile> getTopTen() throws Exception {
         return repository.findTop10ByOrderByCreditsDesc().orElseThrow(Exception::new);
     }
 
+    @Transactional
     public String getCredits(String name) throws Exception {
         return repository.findByName(name)
                 .map(UserProfile::creditsToString)
                 .orElseThrow(Exception::new);
     }
 
-    public UserProfile getProfile(String name) throws Exception {
-        return repository.findByName(name).orElseThrow(Exception::new);
+    @Transactional
+    public UserProfile getProfile(String name) {
+        return repository.findByName(name).orElse(new UserProfile());
     }
 
+    @Transactional
     public boolean checkEntry(String name) {
         return repository.findByName(name).isPresent();
     }
 
+    @Transactional
     public boolean deleteEntry(String name) {
         repository.deleteByName(name);
         return repository.findByName(name).isPresent();
     }
 
+    @Transactional
     public List<UserProfile> getAllProfilesOf(int uid) {
         return repository.findAllByUid(uid).orElse(new ArrayList<>());
     }
